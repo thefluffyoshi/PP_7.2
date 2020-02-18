7bceee06e7d99d1eda7ae2d366116f355a04b1fb
thefluffyoshi <katielynntrinh@gmail.com>
2020-02-18 13:55:05 -0800

    added new variables

--- src/Soccer.java
+++ src/Soccer.java
@@ -2,8 +2,13 @@ public class Soccer
 {
     private String equipment;
     private String team;
-    public Soccer()
+    private int score;
+    private String ball;
+    public Soccer(String equipment, String team, String ball, int score)
     {
-
+        this.equipment = equipment;
+        this.team = team;
+        this.ball = ball;
+        this.score = score;
     }
 }//end soccer
