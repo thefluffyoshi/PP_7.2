30882080be03b15dfb5ff44e918325719200d38a
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2020-03-01 19:06:46 -0800

    prints out the different child classes

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main
 {
     public static void main (String[] args)
     {
-        System.out.println(1);
-    }//end main method
-}//end main
+        Soccer soccer = new Soccer("Soccer", "ball", 31, 11);
+        Badminton badminton = new Badminton("Badminton", "Rackets and birdies", 21, 3, 2);
+        Basketball basketball = new Basketball("Basketball", "Court, hoops and ball", 21, "jerseys, shorts and cleats");
+
+        System.out.println(soccer + "\n------\n");
+        System.out.println(badminton + "\n-----\n");
+        System.out.println(basketball + "\n------\n");
+    }
+}
