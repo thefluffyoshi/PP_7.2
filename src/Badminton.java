c12b98957715cf452f988a372340e9fb60b348c9
thefluffyoshi <54290315+thefluffyoshi@users.noreply.github.com>
2020-03-01 19:04:50 -0800

    extends sport and finished class

--- src/Badminton.java
+++ src/Badminton.java
@@ -1,9 +1,18 @@
-public class Badminton
+public class Badminton extends Sport
 {
-    private int score;
-    private String equipment;
-    public Badminton(int score)
+    private int players;
+    private int games;
+    public Badminton(String sport_Name, String equipment, int score, int games, int players)
     {
+        super(sport_Name, equipment, score);
+        this.games = games;
+        this.players = players;
+    }
 
-    }//end constructor
+    public String toString()
+    {
+        String output = super.toString() + "\nA match consists of a best of: "+ games + "\nThe number of players are: " + players;
+
+        return output;
+    }//end toString
 }//end badminton
